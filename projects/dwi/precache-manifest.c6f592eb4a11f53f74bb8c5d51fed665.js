3fa026fed880b05ec16047758535acb264f6cb54
anedpol <56196147+anedpol@users.noreply.github.com>
2019-12-11 17:26:24 +0200

    Add files via upload

---
+++ projects/dwi/precache-manifest.c6f592eb4a11f53f74bb8c5d51fed665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85563f3fb0c8c905ae57",
+    "url": "/projects/dwi/css/app.674da42a.css"
+  },
+  {
+    "revision": "edf2526704a691d7afa4de826d8aac0e",
+    "url": "/projects/dwi/index.html"
+  },
+  {
+    "revision": "85563f3fb0c8c905ae57",
+    "url": "/projects/dwi/js/app.f02f5d29.js"
+  },
+  {
+    "revision": "4b7b5d7b4d2cf02b0242",
+    "url": "/projects/dwi/js/chunk-vendors.9b3ec7ce.js"
+  },
+  {
+    "revision": "d2a0999d7dfef89aae5d6228f50d824c",
+    "url": "/projects/dwi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/projects/dwi/robots.txt"
+  }
+]);
