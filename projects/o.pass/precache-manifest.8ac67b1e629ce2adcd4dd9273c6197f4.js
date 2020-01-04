91001a57c65c6bcfacf7a859dbc4a9b08839ba8c
anedpol <56196147+anedpol@users.noreply.github.com>
2020-01-04 19:23:23 +0200

    Add files via upload

---
+++ projects/o.pass/precache-manifest.8ac67b1e629ce2adcd4dd9273c6197f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f947c75a0c266a024c2b",
+    "url": "css/app.50b34ec4.css"
+  },
+  {
+    "revision": "aac62dd35173ebdc0079d27561fa2e49",
+    "url": "index.html"
+  },
+  {
+    "revision": "f947c75a0c266a024c2b",
+    "url": "js/app-legacy.2ee333e8.js"
+  },
+  {
+    "revision": "ee018d84ee1d33d10ee9",
+    "url": "js/chunk-vendors-legacy.ff5dac2a.js"
+  },
+  {
+    "revision": "1eb92175680f276f7aec49ebc3ec6324",
+    "url": "manifest.json"
+  }
+]);
